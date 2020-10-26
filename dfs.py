3ed731a211523eaf3031485dda0416b2a2beff36
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2020-10-26 12:54:37 +0000

    Made dfs work, and checked against matlab code

--- dfs.py
+++ dfs.py
@@ -23,24 +23,24 @@ def dfs(time_series):
         print('needs to be 1D')
 
     # Create index of time-series entries
-    j = np.arange(1, N+1)
+    j = np.arange(0, N)
 
     # Calculate the coefficients at each frequenctime_series
-    G = np.zeros((2, int(N-1)/2))
-# 2 = sin, cos    
-# N = number of timesteps (5)    
-# k_max = int(N-1)/2 = maximum harmonic number
+    G = np.zeros((2, int((N-1)/2)))
+    # 2 = sin, cos
+    # N = number of timesteps (5)
+    # k_max = int(N-1)/2 = maximum harmonic number
     alpha0 = np.mean(time_series)
     for k in range(1, 1+int((N-1)/2)):
         C = np.cos(2*np.pi*j*k/N)
         S = np.sin(2*np.pi*j*k/N)
         Z = np.vstack((C, S))
-        #print time_series
-        G[k-1,:] = (2./N) * np.matmul(Z, time_series - alpha0)
+        # print time_series
+        G[k-1, :] = (2./N) * np.matmul(Z, time_series.T - alpha0)
     # Assemble structure containing results
     alpha = G[:, 0]
     beta = G[:, 1]
-    power = 0.5 * (alpha**2 + beta**2)/np.var(time_series)
+    power = 0.5 * (alpha.T**2 + beta.T**2)/np.var(time_series, ddof=1)
     return alpha, beta, power
 
 
