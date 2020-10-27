0ede62565f56ad973c007c99a4a6a2abc6bb5770
Joseph Fishlock <joseph.fishlock@bnc.ox.ac.uk>
2020-10-27 11:15:56 +0000

    docstrings to remove_trend.

--- timeseries_stats.py
+++ timeseries_stats.py
@@ -1,24 +1,32 @@
 # Very rough first attempt at detrending, deseasonalising data
 
-# Expect input as a panda df
+# Expect input as a pandas df
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-#print(os.getcwd())
+# Import temporary test data as pandas df
 df = pd.read_csv(os.getcwd() + '/spino/data/linear_trend_test_timeseries.csv')
 
 
-#print(df.head(n=5))
-
-# Remove trend
 def remove_trend(df, N):
-    #df = ts.df
-    trend_coeffs = np.polyfit(df.time, df.y, deg=N) # gives coefficients
-    fit = np.polyval(trend_coeffs, df.time) # evaluates polynomial fit
+    """Remove a best fitting polynomial of degree N from time series data.
+
+    Uses numpy methods polyfit to find the coefficients of a degree N
+    polynomial of best fit (least squares resiuduals) and polyeval to
+    construct the polynomial over the duration of the time series.
+
+    :param df: Time series data as a pandas dataframe.
+    :param N: Degree of polynomial trend to remove.
+    :return detrended: df.y with the best fitting polynomial subtracted off.
+    :return fit: Array of values of the best fitting polynomial at each time.
+    """
+    trend_coeffs = np.polyfit(df.time, df.y, deg=N)
+    fit = np.polyval(trend_coeffs, df.time)
     detrended = df.y - fit
     return detrended, fit
+
 
 detrended, fit = remove_trend(df, 1)
 
@@ -30,4 +38,3 @@ plt.legend()
 plt.show()
 
 # Remove seasonality of a set period
-
