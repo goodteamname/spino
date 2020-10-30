a347d19ebc11bbb631765c61b9a2208dcf017617
miriamzg <miriamgauntlett@hotmail.co.uk>
2020-10-30 12:41:20 +0000

    Updated correlation tab

--- bokeh_app/main.py
+++ bokeh_app/main.py
@@ -11,6 +11,7 @@ from bokeh.models.widgets import Tabs
 
 # Each tab is drawn by one script
 from scripts.timeseries_copy import timeseries_tab
+from scripts.correlation import correlations_tab
 # from scripts.histogram import histogram_tab
 
 # Read data into dataframes
@@ -21,9 +22,10 @@ hist_ts.set_index('time', inplace=True, drop=True)
 
 # Create each of the tabs
 tab1 = timeseries_tab(ts, hist_ts)
+tab3 = correlations_tab(ts)
 
 # Put all the tabs into one application
-tabs = Tabs(tabs=[tab1])
+tabs = Tabs(tabs=[tab1, tab3])
 
 # Put the tabs in the current document for display
 curdoc().add_root(tabs)
--- bokeh_app/scripts/correlation.py
+++ bokeh_app/scripts/correlation.py
@@ -1,83 +1,156 @@
 # Import packages.
-from .functions/timeseries_stats import auto_corr, corr
+from .functions.timeseries_stats import auto_corr, corr
 from bokeh.plotting import figure
-from bokeh.models import (CategoricalColorMapper, HoverTool, ColumnDataSource, Panel, FuncTickFormatter, SingleIntervalTicker, LinearAxis)
-from bokeh.models.widgets import (CheckboxGroup, Slider, RangeSlider, Tabs, CheckboxButtonGroup, RadioButtonGroup, TableColumn, DataTable, Select, TextInput)
-from bokeh.layouts import column, row, WidgetBox
-from bokeh.palettes import Category20_16
-
-# Set the colour palette from bokeh palettes.
-ts_colors = Category20_16
+from bokeh.models import (HoverTool, ColumnDataSource,
+                          Panel,
+                          LinearAxis)
+from bokeh.models.widgets import (Slider,
+                                  Select, TextInput)
+from bokeh.layouts import WidgetBox, gridplot
 
 
 # Correlations Tab - takes time series dataframe as input, ts.
 def correlations_tab(ts):
 
-    #making dataset using timeseries with default maximum lag of 10
-    def make_dataset(ts[['time', ts_available[0]]]), lag=10):
+    # Making dataset using the autocorrelation function,
+    # input is a timeseries and default maximum lag of 10
+    def make_dataset_autocorr(ts, col, lag):
 
-        df_to_plot = auto_corr(ts, lag)
+        df_to_plot_autocorr = auto_corr(ts[col], lag)
 
-        return ColumnDataSource(df_to_plot) 
+        return ColumnDataSource(df_to_plot_autocorr)
 
-    # Make line plot for raw and detrended data.
+    # Making autocorrelation plot
     def make_autocorrplot(source):
+        ttp = [("Lag", "$x"), ("Autocorrelation", "$y")]
 
-        ts_list = source.column_names
-        ts_list.remove('index')
-        ts_list.remove('time')
-
-        ttp = [("Time", "$x"), ("Value", "$y")]
-
-        plot_autocorr = figure(plot_height=400, plot_width=600, tooltips=ttp, title="Autocorrelation",
-                    tools="hover, pan, zoom_in, zoom_out, reset, save")
-        for i, name in enumerate(ts_list):
-            plot_autocorr.line('time', name, source=source, line_width=3, line_color=ts_colors[i], legend_label=name)
-
-        plot_autocorr.legend.location = "top_left"
-        plot_autocorr.legend.click_policy = "hide"
+        plot_autocorr = figure(plot_height=400, plot_width=600, tooltips=ttp,
+                               title="Autocorrelation",
+                               x_axis_label="Lag", 
+                               y_axis_label="Autocorrelation",
+                               tools="hover, pan, zoom_in, zoom_out, reset, save")
+        plot_autocorr.circle('lags', 'auto_corrs', source=source)
+        plot_autocorr.line('lags', 'auto_corrs', source=source)
+        plot_autocorr.title.text_font_size = '14pt'
+        plot_autocorr.xaxis.axis_label_text_font_size = '12pt'
+        plot_autocorr.xaxis.axis_label_text_font_style = 'bold'
+        plot_autocorr.yaxis.axis_label_text_font_size = '12pt'
+        plot_autocorr.yaxis.axis_label_text_font_style = 'bold'
 
         return plot_autocorr
 
-    # Set up callbacks
+    # Set up callbacks to interactively update data when different
+    # time series and lag values are selected, and letting the user
+    # set the title
     def update_title(attrname, old, new):
-        plot_autocorr.title.text = text.value
+        plot_autocorr.title.text = text_autocorr.value
 
-    def update_data(attrname, old, new):
-        new_source_autocorr = ColumnDataSource(data=ts[['time', ts_select.value]])
-        make_dataset()
+    def update_data_autocorr(attrname, old, new):
+        new_source_autocorr = make_dataset_autocorr(ts,
+                                                    ts_select_autocorr.value,
+                                                    lag=lag_select_autocorr.value)
         source_autocorr.data.update(new_source_autocorr.data)
-        # new_hist_src = make_dataset(hist_ts_to_plot,
-        #                             bin_width=binwidth_select.value)
-        #hist_src.data.update(new_hist_src.data)
+
 
     # Set up widgets.
     # Input for plot titles
-    text = TextInput(title="Title", value='Plot Name')
-    text.on_change('value', update_title)
+    text_autocorr = TextInput(title="Title", value='Autocorrelation')
+    text_autocorr.on_change('value', update_title)
     # Select time series for Autocorrelation
     ts_available = ts.columns.tolist()
+    print(ts_available)
     ts_available.remove('time')
-    ts_select = Select(value=ts_available[0], title='Time Series', options=ts_available)
-    ts_select.on_change('value', update_data)
+    ts_select_autocorr = Select(value=ts_available[0], title='Time Series',
+                                options=ts_available)
+    ts_select_autocorr.on_change('value', update_data_autocorr)
     # Select lag value
-    lag_select = Slider(start=0, end=10, step=1, value=0, title='Lag')
-    lag_select.on_change('value', update_data)
+    lag_select_autocorr = Slider(start=10, end=100, step=1, value=10,
+                                 title='Lag')
+    lag_select_autocorr.on_change('value', update_data_autocorr)
 
     # Initial state and plotting.
+    source_autocorr = make_dataset_autocorr(ts, ts_available[0], 10)
 
-    source_autocorr = ColumnDataSource(data=ts[['time', ts_available[0]]])
-
-    plot_autocorr = make_lineplot_autocorr(source_autocorr)
+    plot_autocorr = make_autocorrplot(source_autocorr)
 
 
     # Set up layouts and add to document.
     # Put controls in a single element.
-    controls = WidgetBox(text, ts_select, component_select)
+    controls_autocorr = WidgetBox(text_autocorr, ts_select_autocorr,
+                                  lag_select_autocorr)
+
+    # making dataset using the correlation function
+    # inputs are two different timeseries and default maximum lag of 10
+    def make_dataset_corr(ts, col1, col2, lag):
+
+        df_to_plot_corr = corr(ts[col1], ts[col2], lag)
+
+        return ColumnDataSource(df_to_plot_corr)
+
+    # Make line plot for correlation
+    def make_corrplot(source):
+        ttp = [("Lag", "$x"), ("Autocorrelation", "$y")]
+
+        plot_corr = figure(plot_height=400, plot_width=600, tooltips=ttp,
+                           title="Correlation",
+                           x_axis_label="Lag", y_axis_label="Correlation",
+                           tools="hover, pan, zoom_in, zoom_out, reset, save")
+        plot_corr.circle('lags', 'corrs', source=source)
+        plot_corr.line('lags', 'corrs', source=source)
+        plot_corr.title.text_font_size = '14pt'
+        plot_corr.xaxis.axis_label_text_font_size = '12pt'
+        plot_corr.xaxis.axis_label_text_font_style = 'bold'
+        plot_corr.yaxis.axis_label_text_font_size = '12pt'
+        plot_corr.yaxis.axis_label_text_font_style = 'bold'
+
+        return plot_corr
+
+    # Set up callbacks to interactively update data when different
+    # time series and lag values are selected, and letting the user
+    # set the title
+    def update_title(attrname, old, new):
+        plot_corr.title.text = text_corr.value
+
+    def update_data_corr(attrname, old, new):
+        new_source_corr = make_dataset_corr(ts, ts_select_corr.value,
+                                            ts_select2_corr.value,
+                                            lag=lag_select_corr.value)
+        source_corr.data.update(new_source_corr.data)
+
+
+    # Set up widgets.
+    # Input for plot titles
+    text_corr = TextInput(title="Title", value='Correlation')
+    text_corr.on_change('value', update_title)
+    # Select time series for Autocorrelation
+    ts_available = ts.columns.tolist()
+    ts_available.remove('time')
+    ts_select_corr = Select(value=ts_available[0], title='Time Series',
+                            options=ts_available)
+    ts_select_corr.on_change('value', update_data_corr)
+    ts_select2_corr = Select(value=ts_available[1], title='Time Series',
+                             options=ts_available)
+    ts_select2_corr.on_change('value', update_data_corr)
+    # Select lag value
+    lag_select_corr = Slider(start=10, end=100, step=1, value=10, title='Lag')
+    lag_select_corr.on_change('value', update_data_corr)
+
+    # Initial state and plotting.
+    source_corr = make_dataset_corr(ts, ts_available[0], ts_available[1], 10)
+
+    plot_corr = make_corrplot(source_corr)
+
+
+    # Set up layouts and add to document.
+    # Put controls in a single element.
+    controls_corr = WidgetBox(text_corr, ts_select_corr,
+                              ts_select2_corr, lag_select_corr)
 
     # Create a row layout
-    layout = row(controls, plot_autocorr)
+    grid = gridplot([[controls_autocorr, plot_autocorr],
+                     [controls_corr, plot_corr]],
+                    plot_width=500, plot_height=500)
 
     # Make a tab with the layout.
-    tab = Panel(child=layout, title='Autocorrelation and Correlation')
+    tab = Panel(child=grid, title='Autocorrelation and Correlation')
     return tab
